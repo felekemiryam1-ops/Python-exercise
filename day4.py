14520c5bf60f07b943c65d41aa0586d95971201e
felekemiryam1-ops <felekemiryam1@gmail.com>
2026-07-30 13:41:02 -0400

    day 4

---
+++ day4.py
@@ -0,0 +1,11 @@
+
+#synatax error we must fix it
+#run time error happen when the code is running
+
+try:
+    x = int(input(" what is x"))
+    print(f"x is {x}")
+
+except ValueError:
+     print("x is not integer")
+
