3aed8f3cd7855cbe85146d8bf770d631b4e625fb
felekemiryam1-ops <felekemiryam1@gmail.com>
2026-08-04 18:20:01 -0400

    Exception

--- day4.py
+++ day4.py
@@ -1,11 +1,20 @@
 
 #synatax error we must fix it
 #run time error happen when the code is running
+def main():
+   x=get_int("what is x")
+   print(f"x is {x}")
 
-try:
-    x = int(input(" what is x"))
-    print(f"x is {x}")
+def get_int(prompt) : 
+  while True:
+    try:
+      x = int(input(prompt))
+      return x
 
-except ValueError:
-     print("x is not integer")
+    except ValueError:
+        pass#print("x is not integer")
+    
 
+
+
+main()
---
+++ exception.py
@@ -0,0 +1,46 @@
+
+#Exercise 1
+
+"""def main():
+    x = get_int("enter a number")
+    print(f"you entered: {x}")
+
+
+def get_int(prompt):
+    while True:
+        try:
+            x = int(input(prompt))
+            print(f"you entered{x}")
+            return x
+        except ValueError:
+            print("Invalid number")
+
+
+main()"""
+
+
+#exercise 2
+
+"""while True:
+    try:
+       
+       x=int(input("number 1?")) 
+       y=int(input("number 2?"))
+      
+       print(f"Result: {x/y}")
+       break
+    except ZeroDivisionError:
+        print("u can't divide by 0")"""
+
+
+#exercise 3
+
+try:
+ print(age)
+except NameError:
+ print("no name")
+
+
+ 
+
+
